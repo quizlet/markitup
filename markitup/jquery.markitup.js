50ffa432af230f5e5f8d448fdaaa7e41e78907e5
Jeff Chan <jefftchan@gmail.com>
2015-12-13 02:05:27 -0800

    Get rid of AMD

--- markitup/jquery.markitup.js
+++ markitup/jquery.markitup.js
@@ -25,12 +25,9 @@
 // THE SOFTWARE.
 // ----------------------------------------------------------------------------
 (function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(['jquery'], factory);
-	} else if (typeof exports === 'object') {
-		// Node/CommonJS
-		factory(require('jquery'));
+	if (typeof exports === 'object') {
+		// Node/CommonJS style for Browserify
+		module.exports = factory;
 	} else {
 		// Browser globals
 		factory(jQuery);
